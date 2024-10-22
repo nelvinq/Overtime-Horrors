5765d4a03b37521c32de49fff9d78c9347687c40
Nelvin <nelvinq@gmail.com>
2024-10-22 21:57:52 +0800

    feat: add in css and html content

---
+++ app.js
@@ -0,0 +1,25 @@
+/*-------------------------------- Constants --------------------------------*/
+
+const game{
+	currentPath:""
+	choiceA:""
+	choiceB:""
+	gameover:""
+}
+
+const paths{
+	[path = 1, pathImage= "img", pathStory="Path 1 Text", choiceA="Path 1 A Choice", choiceApath=2, choiceB="Path 1 B Choice", choiceBpath=3, gameover ="false"]
+	[path = 2, pathImage= "img", pathStory="Text", choiceA="Text", choiceApath=4, choiceB="Text", choiceBpath=5, gameover ="false"]
+	[path = 3, pathImage= "img", gameover ="true"]
+}
+
+/*-------------------------------- Variables --------------------------------*/
+
+
+/*------------------------ Cached Element References ------------------------*/
+
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+
+/*----------------------------- Functions -----------------------------*/
